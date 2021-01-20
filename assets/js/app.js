ce64b9798aa31a77e78b641bd39eb19e7c9fabfd
= <developer@cluebixsoftware.com>
2021-01-20 17:20:33 +0530

    Final home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,69 @@
+$('.js-tilt').tilt({
+  glare: true,
+  maxGlare: .5,
+  scale:1.04,
+})
+$('.service').tilt({
+  glare: true,
+  maxGlare: .5,
+  scale:1.04,
+  perspective:1000
+})
+
+// Tab script for feedback Section
+function openFeedback(evt, id) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(id).style.display = "block";
+  evt.currentTarget.className += " active";
+  $('.'+id).show();
+}
+document.getElementById("defaultOpen").click();
+
+// Slider for project section
+var owl = $(".owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: false,
+  items: 1,
+  nav:true,
+  autoplayHoverPause: true,
+  animateIn: 'fadeInUp',
+  animateOut: 'fadeOutDown',
+  responsive:{
+    0:{
+      dots:true
+    },
+    780:{
+      dots:false
+    }
+  }
+});
+$('.custom-up').click(function() {
+  owl.trigger('next.owl.carousel');
+  owl.animateIn
+})  
+$('.custom-down').click(function() {
+  owl.trigger('prev.owl.carousel');
+}) 
+
+
+
+
+// Sidebar on mobile view
+$('.open-menu-btn').click(function() {
+  $('.c-nav-links').addClass('open')
+})  
+$('.close-btn').click(function() {
+  $('.c-nav-links').removeClass('open')
+})  
+
 // intersection observer for Counter animation
 let count = document.querySelector('.counts');
 if(count){
@@ -28,5 +94,24 @@ if(count){
 
       updateCount();
     });
+  }
+}
+
+// intersection observer for Blue section fingerprint SVG animation
+let thumb = document.querySelector('.thumb');
+if(thumb){
+  let observer2 = new IntersectionObserver((entries, observer) =>{
+    entries.forEach(entry =>{
+      if(entry.isIntersecting){
+        setTimeout(function(){ rotate_pattern(35); }, 500);
+      }
+      else{
+        setTimeout(function(){ rotate_pattern(0); }, 100);
+      }
+    });
+  },{threshold:0.45});
+  observer2.observe(thumb);
+  function rotate_pattern(deg){
+    thumb.style.transform = "rotate("+deg+"deg)";
   }
 }
