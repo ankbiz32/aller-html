e4bd9bcc00ca16838800a1c5f671414e46836c29
= <developer@cluebixsoftware.com>
2021-01-21 18:19:04 +0530

    Design

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,21 +11,23 @@ $('.service').tilt({
 })
 
 // Tab script for feedback Section
-function openFeedback(evt, id) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+if(document.getElementById("defaultOpen")){
+  function openFeedback(evt, id) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(id).style.display = "block";
+    evt.currentTarget.className += " active";
+    $('.'+id).show();
   }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(id).style.display = "block";
-  evt.currentTarget.className += " active";
-  $('.'+id).show();
+  document.getElementById("defaultOpen").click();
 }
-document.getElementById("defaultOpen").click();
 
 // Slider for project section
 var owl = $(".owl-carousel").owlCarousel({
